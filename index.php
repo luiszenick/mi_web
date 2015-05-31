e329eec8ac9e40c91f1f5aa35092c1acd0fa79d6
Luis <Luis@TELMEX.lan>
2015-05-31 18:26:07 -0500

    primera prueba gith

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+      <form action="login.php" method="post">
+                  
+                  
+                  <div align="left">
+                    <table align="left">
+                        
+                      <label>
+                      <div align="center">Usuario </div>
+                      </label>
+                      <div align="center">
+                        <input class="inputfield" name="user" type="text" />
+                        <br>
+                      </div>
+                          </br>
+                        
+                      <label>
+                      <div align="center">Password </div>
+                      </label>
+                      <div align="center">
+                        <input class="inputfield" name="pw" type="password" /> <br>
+                        <input  type="submit"  value="Enviar" />
+                      </div>
+                    </table>
+                  </div>
+                </form> 
+    </body>
+</html>
